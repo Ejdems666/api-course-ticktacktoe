cb19c4d3275ed090114956769578417d56aac218
Adam Becvar <becvar.adam666@gmail.com>
2018-01-30 09:47:32 +0100

    Add template for Board toString

--- src/main/java/org/cba/api/internal/game/StandardBoard.java
+++ src/main/java/org/cba/api/internal/game/StandardBoard.java
@@ -1,8 +1,15 @@
 package org.cba.api.internal.game;
 
 import org.cba.api.internal.Board;
+import org.cba.api.internal.Position;
+import org.cba.api.internal.Symbol;
+
+import java.util.HashMap;
+import java.util.List;
 
 public class StandardBoard implements Board {
+
+    private HashMap<Position,Symbol> positions;
 
     @Override
     public int getXSize() {
@@ -12,5 +19,15 @@ public class StandardBoard implements Board {
     @Override
     public int getYSize() {
         return 10;
+    }
+
+    @Override
+    public String toString() {
+        for (int x = 0; x < getXSize(); x++) {
+            for (int y = 0; y < getYSize(); y++) {
+                Symbol symbol = positions.get(new Position(x,y));
+            }
+        }
+        return "";
     }
 }
