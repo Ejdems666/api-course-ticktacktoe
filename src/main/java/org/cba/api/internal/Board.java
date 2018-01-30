097f3b8062fa1b4efe4cc907faffe70fa0a98043
Adam Becvar <becvar.adam666@gmail.com>
2018-01-30 09:32:39 +0100

    Delete AI related methods from boards

--- src/main/java/org/cba/api/internal/Board.java
+++ src/main/java/org/cba/api/internal/Board.java
@@ -3,22 +3,6 @@ package org.cba.api.internal;
 import java.util.List;
 
 public interface Board {
-    List<Position> getPositionsToTheLeft(Position position);
-
-    List<Position> getPositionsToTheRight(Position position);
-
-    List<Position> getPositionsToTheTop(Position position);
-
-    List<Position> getPositionsToTheBottom(Position position);
-
-    List<Position> getPositionsToTheBottomLeft(Position position);
-
-    List<Position> getPositionsToTheBottomRight(Position position);
-
-    List<Position> getPositionsToTheTopLeft(Position position);
-
-    List<Position> getPositionsToTheTopRight(Position position);
-
     int getXSize();
 
     int getYSize();
--- src/main/java/org/cba/api/internal/game/StandardBoard.java
+++ src/main/java/org/cba/api/internal/game/StandardBoard.java
@@ -1,50 +1,8 @@
 package org.cba.api.internal.game;
 
 import org.cba.api.internal.Board;
-import org.cba.api.internal.Position;
 
-import java.util.List;
-
-public class StandardBoard implements Board{
-    @Override
-    public List<Position> getPositionsToTheLeft(Position position) {
-        return null;
-    }
-
-    @Override
-    public List<Position> getPositionsToTheRight(Position position) {
-        return null;
-    }
-
-    @Override
-    public List<Position> getPositionsToTheTop(Position position) {
-        return null;
-    }
-
-    @Override
-    public List<Position> getPositionsToTheBottom(Position position) {
-        return null;
-    }
-
-    @Override
-    public List<Position> getPositionsToTheBottomLeft(Position position) {
-        return null;
-    }
-
-    @Override
-    public List<Position> getPositionsToTheBottomRight(Position position) {
-        return null;
-    }
-
-    @Override
-    public List<Position> getPositionsToTheTopLeft(Position position) {
-        return null;
-    }
-
-    @Override
-    public List<Position> getPositionsToTheTopRight(Position position) {
-        return null;
-    }
+public class StandardBoard implements Board {
 
     @Override
     public int getXSize() {
